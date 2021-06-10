b97af19641252666a34651c570ae32667a031dd1
djimenez97 <84532231+djimenez97@users.noreply.github.com>
2021-06-10 14:00:22 -0400

    2.4 Jquery

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,10 @@
 console.log("Hey there, Welcome")
+
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+    if (x.className === 'navtoggle') {
+      x.className += ' responsive';
+    } else {
+      x.className = 'navtoggle';
+    }
+  }
